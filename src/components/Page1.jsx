f296af3b14173de4796422d7b704204f5f963332
AMV0027 <129928410+AMV0027@users.noreply.github.com>
2024-02-22 16:12:03 +0530

    Modified IT name

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -5,7 +5,7 @@ function Page1() {
     <div className='bg-gray-900 text-white' style={{height:"120vh", maxWidth:"100%"}} >
         <div className="flex justify-center">
             <div className="typo-backgroung px-5 pt-20 leading-none">
-                SREC IT <br /> XTENSIS<br /> IT DEPT<br /> PHOENIX
+                SREC IT <br /> XTENSIS<br /> IT  DEPT<br /> PHOENIX
             </div>
         </div>
     </div>
